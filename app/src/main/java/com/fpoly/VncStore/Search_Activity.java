6215316a0469d2ee0e63bd47d06559b9273659e9
Giap Van Thanh Dat <datgvtdev03@gmail.com>
2022-11-21 12:57:01 +0700

    gan hoan thien search

--- app/src/main/java/com/fpoly/VncStore/Search_Activity.java
+++ app/src/main/java/com/fpoly/VncStore/Search_Activity.java
@@ -5,29 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
 import android.widget.SearchView;
 
+import com.fpoly.VncStore.Model.Sanpham;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.sql.DatabaseMetaData;
+import java.util.ArrayList;
 
 public class Search_Activity extends AppCompatActivity {
 
+    Sanpham sanpham;
     DatabaseReference databaseReference;
-    private SearchView searchView;
-    private RecyclerView recyclerView;
+    private AutoCompleteTextView txt_Search;
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
         databaseReference = FirebaseDatabase.getInstance().getReference("SanPham");
-        searchView = findViewById(R.id.search_id);
-        recyclerView = findViewById(R.id.rcy_id);
+        txt_Search = findViewById(R.id.search_id);
+        listView = findViewById(R.id.lv_id);
 
         ValueEventListener eventListener = new ValueEventListener() {
             @Override
@@ -40,8 +50,56 @@ public class Search_Activity extends AppCompatActivity {
 
             }
         };
+
+        databaseReference.addListenerForSingleValueEvent(eventListener);
     }
 
     private void populateSearch(DataSnapshot snapshot) {
+        ArrayList<String> nameSP = new ArrayList<>();
+        if(snapshot.exists()) {
+            for (DataSnapshot dataSnapshot:snapshot.getChildren()) {
+                String names = dataSnapshot.child("name").getValue(String.class);
+                nameSP.add(names);
+            }
+
+            ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, nameSP);
+            txt_Search.setAdapter(arrayAdapter);
+            txt_Search.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                    String name = txt_Search.getText().toString();
+                    searchName(name);
+                }
+            });
+
+        } else {
+            Log.d("SanPham", "Khong co du lieu");
+        }
+    }
+
+    private void searchName(String name) {
+        Query query = databaseReference.orderByChild("name").equalTo(name);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if(snapshot.exists()) {
+                    ArrayList<String> listSanPham = new ArrayList<>();
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        listSanPham = new ArrayList<>();
+                        sanpham = new Sanpham(dataSnapshot.child("name").getValue(String.class), dataSnapshot.child("Gia").getValue(String.class), dataSnapshot.child("image").getValue(String.class), dataSnapshot.child("khuyenmai").getValue(String.class), dataSnapshot.child("moTa").getValue(String.class));
+                        listSanPham.add(sanpham.getName() + "\n" + sanpham.getGia() + "\n" + sanpham.getImage() + "\n" + sanpham.getKhuyenmai() + "\n" + sanpham.getMoTa());
+                    }
+                    ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1, listSanPham);
+                    listView.setAdapter(arrayAdapter);
+                } else {
+                    Log.d("SanPham", "ko co san pham");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
